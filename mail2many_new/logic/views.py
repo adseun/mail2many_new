be8de53dad86c466c8f06c52ea272610c8d10a81
okedeji oluwatofunmi <oketofoke@gmail.com>
2022-07-13 22:06:52 +0100

    docer config

--- mail2many_new/logic/views.py
+++ mail2many_new/logic/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 # Create your views here.
 from rest_framework.response import Response
-import pandas as pd
 
 from .serializers import EmailListUploadSerializer, EmailReceipientRelationshipSerializer
 from .models import EmailListUpload, EmailReceipientRelationship
