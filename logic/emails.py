df984a60f43baa10a6896037c13efbb0286fe400
adeshinafuad <adeshinafuad@gmail.com>
2022-08-31 11:34:10 +0100

    change device

--- logic/emails.py
+++ logic/emails.py
@@ -1,4 +1,5 @@
 from django.core.mail import send_mass_mail
+import pandas as pd
 
 def send_email(emails, names):
     sender_email = "mail2many@gmail.com"
@@ -12,5 +13,11 @@ def send_email(emails, names):
     )
     
     return True
+def read_csv(csv_link):
+    df = pd.read_csv('namesandemails.csv')
+    names = df.loc[:, 'name'].to_list()
+    emails = df.loc[:, 'email'].to_list()
+    messages = df.loc[:, 'message'].to_list()
+    return names, emails
     
         
--- logic/views.py
+++ logic/views.py
@@ -1,58 +1,84 @@
-import email
-from urllib import response
 from django.shortcuts import render
 from rest_framework.views import APIView
-# Create your views here.
 from rest_framework.response import Response
-import csv
-
-from logic.emails import send_email
-
+from .emails import send_email, read_csv
 from .serializers import EmailListUploadSerializer, EmailReceipientRelationshipSerializer
 from .models import EmailListUpload, EmailReceipientRelationship
 from rest_framework.permissions import AllowAny
-
-
-
 from rest_framework import status
+import pandas as pd
+
+
+
+
 class SendEmailAPIView(APIView):
     permission_classes=(AllowAny,)
     
-    def post(self,request,*args,**kwargs):
+    def post(self, request, *args, **kwargs):
         serializer = EmailListUploadSerializer(data=request.data)
-        # file_type=request.data["file_type"]
-        file_type= "https://res.cloudinary.com/mail2many/raw/upload/v1657322583/ABAJI_bms6aw.xlsx"
-        if file_type != "xlsx" or file_type != "csv":
+        file_type=request.data["file_type"]
+        
+        if file_type != "xlsx" or "csv":
             return Response(data={"message":"File type unacceptable"}, status=status.HTTP_403_FORBIDDEN)
         
         if serializer.is_valid():
             serializer.save()
             spreadsheet=request.data["spreadsheet"]
-            if file_type == "xlsx":
-                pass
-            elif file_type == "csv":
-                with open(spreadsheet, 'r') as file:
-                    csvreader = csv.reader(file)
-                    names = []
-                    emails = []
-                    row_count = 0
-                    for row in csvreader:  
-                        if row_count == 0:
-                            row_count += 1
-                        else:        
-                            names.append(row[1])
-                            emails.append(row[0])
-                            row_count +=1
-                    send_mail=send_email(emails, names) 
-                    
-                return Response(data={"message":"Excel Uploaded successfully", "data":serializer.data}, status=status.HTTP_201_CREATED)
-                        
-            else:
-                return Response(data={"message":"File type unacceptable"}, status=status.HTTP_403_FORBIDDEN)
+            a = read_csv(spreadsheet)
+            print(a)
+            
+            
+            
+    def read_csv(csv_link):
+        df = pd.read_csv("https://res.cloudinary.com/mail2many/raw/upload/v1657322583/ABAJI_bms6aw.xlsx")
+        names = df.loc[:, 'name'].to_list()
+        emails = df.loc[:, 'email'].to_list()
+        messages = df.loc[:, 'message'].to_list()
+        return names, emails
+            
+                            
 
-        else:
-            print(serializer.errors)
-            return Response(data={"message":serializer.errors})
+        
+        
+        
+
+    
+    # def post(self,request,*args,**kwargs):
+    #     serializer = EmailListUploadSerializer(data=request.data)
+    #     # file_type=request.data["file_type"]
+    #     file_type= "https://res.cloudinary.com/mail2many/raw/upload/v1657322583/ABAJI_bms6aw.xlsx"
+    #     if file_type != "xlsx" or file_type != "csv":
+    #         return Response(data={"message":"File type unacceptable"}, status=status.HTTP_403_FORBIDDEN)
+        
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         spreadsheet=request.data["spreadsheet"]
+    #         if file_type == "xlsx":
+    #             pass
+    #         elif file_type == "csv":
+    #             with open(spreadsheet, 'r') as file:
+    #                 csvreader = csv.reader(file)
+    #                 names = []
+    #                 emails = []
+    #                 row_count = 0
+    #                 for row in csvreader:  
+    #                     if row_count == 0:
+    #                         row_count += 1
+    #                     else:        
+    #                         names.append(row[1])
+    #                         emails.append(row[0])
+    #                         row_count +=1
+    #                 send_mail=send_email(emails, names)
+
+                    
+            #     return Response(data={"message":"Excel Uploaded successfully", "data":serializer.data}, status=status.HTTP_201_CREATED)
+                        
+            # else:
+            #     return Response(data={"message":"File type unacceptable"}, status=status.HTTP_403_FORBIDDEN)
+
+        # else:
+        #     print(serializer.errors)
+        #     return Response(data={"message":serializer.errors})
         
         
         
@@ -60,10 +86,10 @@ class SendEmailAPIView(APIView):
         # send_email_function = self.send_email_function
         # return Response(data={"message":"Category created"}, status=status.HTTP_201_CREATED)
     
-    def send_email_function(self):       
-        df = pd.read_csv('https://res.cloudinary.com/mail2many/raw/upload/v1657322583/ABAJI_bms6aw.xlsx')
-        name = df['name'].values
-        email = df['email'].values
+    # def send_email_function(self):       
+    #     df = pd.read_csv('https://res.cloudinary.com/mail2many/raw/upload/v1657322583/ABAJI_bms6aw.xlsx')
+    #     name = df['name'].values
+    #     email = df['email'].values
     
     
     def get(self, request, *args, **kwargs):
@@ -96,7 +122,7 @@ class SendEmailDetailView(APIView):
     def delete(self, request, id, *args, **kwargs):
         email = EmailListUpload.objects.get(id=id)
         email.delete()
-        return Response(status =status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
         
         
         
